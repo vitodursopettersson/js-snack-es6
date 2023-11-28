0eaf3f9d0af339b28b5bc8ce721c8ed421848e63
vitodursopettersson <vitodurso98@gmail.com>
2023-11-28 09:38:43 +0100

    first commit

---
+++ snack1/main.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const raceBike = [
+    {
+        name: 'Canyon Ultimate CF Slx 9.0',
+        weight: 6,
+        photo: 'canyon.jpg',
+    },
+    {
+        name: 'Rose X-Lite Six Disc',
+        weight: 6,
+        photo: 'rose.jpg',
+    },
+    {
+        name: 'Sarto asola',
+        weight: 5,
+        photo: 'sarto.jpg',
+    },
+    {
+        name: 'Bianchi Specialissima',
+        weight: 7,
+        photo: 'bianchi.jpg',
+    },
+    {
+        name: 'Scott Addict RC Premium Disc',
+        weight: 6,
+        photo: 'scott.jpg',
+    },
+]
+
